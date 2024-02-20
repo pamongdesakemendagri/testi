965103498cd0613544105e4e0dad55939a502326
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-21 00:00:13 +0700

    cuba

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 import {getValueRadio} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
+import {get} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
-let pil=getValueRadio("rating");
-console.log(pil);
+get("https://mrt.ulbi.ac.id/notif/ux/getlaporan/"+getHash(),runafterGet)
+
+function runafterGet(result){
+    console.log(result);
+    let pil=getValueRadio("rating");
+    console.log(pil);
+}
+
