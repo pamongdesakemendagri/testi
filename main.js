f53f46dacff0f9cb28d279d7c08d4106402e5958
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-21 06:03:35 +0700

    sow

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-import {getValueRadio,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {getValueRadio,setInner,onClick,hide,show} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {get} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
+
+hide("tombol");
 get("https://mrt.ulbi.ac.id/notif/ux/getlaporan/"+getHash(),runafterGet)
+
+document.getElementsByName("rating").onchange = function() {runOnChangeRating()};
 
 function runafterGet(result){
     console.log(result);
     setInner("petugas",result.petugas);
     setInner("solusi",result.solusi);
-    let pil=getValueRadio("rating");
-    console.log(pil);
 }
 
+function runOnChangeRating(){
+    let pil=getValueRadio("rating");
+    console.log(pil);
+    show("tombol");
+}
