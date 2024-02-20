ad29adf578cdca467a58d4d9159105ea7093c715
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-21 00:20:14 +0700

    oke

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {getValueRadio} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import {getValueRadio,setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {get} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
@@ -6,6 +6,8 @@ get("https://mrt.ulbi.ac.id/notif/ux/getlaporan/"+getHash(),runafterGet)
 
 function runafterGet(result){
     console.log(result);
+    setInner("nama",result.nama);
+    setInner("solusi",result.solusi);
     let pil=getValueRadio("rating");
     console.log(pil);
 }
