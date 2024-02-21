eb13c6e5ef6c851f1edd513d4b365093ed636ef8
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-21 09:57:41 +0700

    postWithToken

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {getValueRadio,setInner,onClick,hide,show} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
-import {get} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {get,postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 
 
 get("https://mrt.ulbi.ac.id/notif/ux/getlaporan/"+getHash(),runafterGet)
